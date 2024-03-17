b09f29bed106af5556909ba95e49abe50d09f9de
sraakesh95github <raakesh95jobs@gmail.com>
2024-03-17 11:34:48 -0400

    feature-1

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import LandingPage from './home/page';
+import SignalIntegrityVerification from './landing/page';
 
 export default function Page() {
-  return <LandingPage />;
+  return <SignalIntegrityVerification />;
 }
