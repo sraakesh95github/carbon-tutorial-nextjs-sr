4b6f0ca39812e598be253748db02602ba9a1ca4c
sraakesh95github <raakesh95jobs@gmail.com>
2024-03-17 11:41:31 -0400

    feat(first page): file uploader 1

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -1,11 +1,7 @@
+'use client';
+
 import React from 'react';
-import {
-  FileUploader,
-  Dropdown,
-  Button,
-  Grid,
-  Column,
-} from '@carbon/react';
+import { FileUploader, Dropdown, Button, Grid, Column } from '@carbon/react';
 
 function SignalIntegrityVerification() {
   // Define the options for your Dropdown component
@@ -17,39 +13,36 @@ function SignalIntegrityVerification() {
   return (
     <Grid className="landing-page" fullWidth>
       <Column lg={16} md={8} sm={4} className="landing-page__banner">
-      <div className="container">
-      <header className="header">
-        <img src="/ford-logo.png" alt="Ford Logo" />
-        <h1>Signal Integrity Verification</h1>
-      </header>
-      <div className="upload-section">
-        <h2>Upload VUT file</h2>
-        <p>Max file size is 500kb. Supported file type is .csv</p>
-        <FileUploader
-          buttonLabel="Upload"
-          filenameStatus="edit"
-          iconDescription="Clear file"
-        />
-        <div className="file-name">P708N_2025_Chicago_CAP_VUT.csv</div>
-      </div>
-      <div className="ecu-dropdown">
-        <Dropdown
-          id="ecu-dropdown"
-          label="Select ECU"
-          titleText="Select ECU"
-          helperText=""
-          items={ecuOptions}
-          itemToString={(item) => (item ? item.label : '')}
-        />
-      </div>
-      <div className="analysis-section">
-        <Button>Begin Analysis</Button>
-      </div>
-    </div>
+        <div className="container">
+          <header className="header">
+            <img src="/ford-logo.png" alt="Ford Logo" />
+            <h1>Signal Integrity Verification</h1>
+          </header>
+          <div className="upload-section">
+            <h2>Upload VUT file</h2>
+            <p>Max file size is 500kb. Supported file type is .csv</p>
+            <FileUploader
+              buttonLabel="Upload"
+              filenameStatus="edit"
+              iconDescription="Clear file"
+            />
+          </div>
+          <div className="ecu-dropdown">
+            <Dropdown
+              id="ecu-dropdown"
+              label="Select ECU"
+              titleText="Select ECU"
+              helperText=""
+              items={ecuOptions}
+              itemToString={(item) => (item ? item.label : '')}
+            />
+          </div>
+          <div className="analysis-section">
+            <Button>Begin Analysis</Button>
+          </div>
+        </div>
       </Column>
     </Grid>
-
-
   );
 }
 
